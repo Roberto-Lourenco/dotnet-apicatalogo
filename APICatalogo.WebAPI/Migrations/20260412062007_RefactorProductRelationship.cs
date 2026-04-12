e0b0ce0679453424ca9601fb4cab0cfb2f03a30a
Roberto Lourenço <169873970+Roberto-Lourenco@users.noreply.github.com>
2026-04-12 03:27:53 -0300

    feat(migrations): aplica migracao de alteracao ondelete entre products e categories

---
+++ APICatalogo.WebAPI/Migrations/20260412062007_RefactorProductRelationship.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorProductRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_products_categories_category_id",
+                schema: "core",
+                table: "products");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_products_categories_category_id",
+                schema: "core",
+                table: "products",
+                column: "category_id",
+                principalSchema: "core",
+                principalTable: "categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_products_categories_category_id",
+                schema: "core",
+                table: "products");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_products_categories_category_id",
+                schema: "core",
+                table: "products",
+                column: "category_id",
+                principalSchema: "core",
+                principalTable: "categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
